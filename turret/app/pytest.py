e0ff0e9631f8056fb2bddf951d3f8e66ba3350c5
Masaki Yatsu <yatsu@yatsu.info>
2018-02-22 11:19:47 +0800

    Use regex instead of fnmatch for test file matching

--- turret/app/pytest.py
+++ turret/app/pytest.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 
-import fnmatch
 from pathlib import Path
 import pytest
 from _pytest import config
@@ -43,18 +42,20 @@ class PyTestRunnerApp(BaseTurretApp):
         self.log.debug('event: %s', event)
         src_path = event['src_path']
 
-        if any([fnmatch.fnmatchcase(src_path, p) for p in self.auto_test]):
-            self.uncache_modules(self.uncache)
-            self.test(src_path)
+        for glob in self.auto_test:
+            regex = self.glob_to_regex(glob)
+            match = re.match(regex, src_path)
+            self.log.debug('auto_test glob: %s regex: %s src_path: %s match: %s',
+                           glob, regex, src_path, match.groups() if match else None)
+            if match:
+                self.uncache_modules(self.uncache)
+                self.test(src_path)
 
         for glob, target in self.auto_test_map.items():
-            # The pattern '...(?!\?\))' assumes that '*' is not followed by '?)'
-            # because '?' and parenthesies are not allowed in the glob syntax
-            pattern = re.sub(r'/', r'\/', re.sub(r'(?<!\\)\*(?!\?\))', r'([^/]*?)',
-                                                 re.sub(r'(?<!\\)\*\*\/?', r'(.*?)', glob)))
-            match = re.match(pattern, src_path)
-            self.log.debug('glob: %s pattern: %s src_path: %s match: %s',
-                           glob, pattern, src_path, match.groups() if match else False)
+            regex = self.glob_to_regex(glob)
+            match = re.match(regex, src_path)
+            self.log.debug('auto_test_map glob: %s regex: %s src_path: %s match: %s',
+                           glob, regex, src_path, match.groups() if match else None)
             if match:
                 target_path = Path(target.format(*match.groups()).replace('//', '/'))
                 if target_path.exists():
@@ -65,3 +66,9 @@ class PyTestRunnerApp(BaseTurretApp):
     def test(self, target):
         self.log.debug('pytest.main %s', self.args + [target])
         pytest.main(self.args + [target])
+
+    def glob_to_regex(self, glob):
+        # The pattern '...(?!\?\))' assumes that '*' is not followed by '?)'
+        # because '?' and parenthesies are not allowed in the glob syntax
+        return re.sub(r'/', r'\/', re.sub(r'(?<!\\)\*(?!\?\))', r'([^/]*?)',
+                                          re.sub(r'(?<!\\)\*\*\/?', r'(.*?)', glob)))
